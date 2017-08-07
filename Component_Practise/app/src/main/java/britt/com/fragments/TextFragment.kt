d584e0095d7e92fcc74ec9a6efa9409ebfd82113
brittvarnom <brittany.varnom@bbc.co.uk>
2017-08-07 09:25:35 +0100

    Reformatting code

--- Component_Practise/app/src/main/java/britt/com/fragments/TextFragment.kt
+++ Component_Practise/app/src/main/java/britt/com/fragments/TextFragment.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class TextFragment: Fragment() {
+class TextFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater?.inflate(R.layout.text_fragment, container, false)
     }
