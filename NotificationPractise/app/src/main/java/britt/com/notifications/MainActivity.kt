d7d49ca0f75f4ddff356323a2b06d9f865fe747e
brittvarnom <brittany.varnom@bbc.co.uk>
2017-08-07 11:52:29 +0100

    Created layout with button and snackbar

--- NotificationPractise/app/src/main/java/britt/com/notifications/MainActivity.kt
+++ NotificationPractise/app/src/main/java/britt/com/notifications/MainActivity.kt
@@ -2,11 +2,22 @@ package britt.com.notifications
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sendNotificationOnButtonClick()
+    }
+
+    fun sendNotificationOnButtonClick() {
+        button_receive_notifications.setOnClickListener {
+            Snackbar.make(it,
+                "Notifications not set up yet. This message will be displayed insstead.",
+                Snackbar.LENGTH_LONG).show()
+        }
     }
 }
